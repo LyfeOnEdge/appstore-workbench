4974f6f3b9ca6d0df504e17d1e159797c83de2f1
LyfeOnEdge <lyfeshomebrew@gmail.com>
2020-02-10 00:35:07 -0800

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import sys
 if __name__ == "__main__":
 	sys.exit("This file was not meant to be run, try running appstoreworkbench.py")
 
-REPO_URL = "https://www.wiiubru.com/" #Uncomment this line for the wii u repo
-# REPO_URL = "https://www.switchbru.com/" 
+# REPO_URL = "https://www.wiiubru.com/" #Uncomment this line for the wii u repo
+REPO_URL = "https://www.switchbru.com/" 
 
 REPO_JSON_URL = f"{REPO_URL}appstore/repo.json"
